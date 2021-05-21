5d06fd2596ec0d46e52fbd5f4b998112147c99a6
changhoonhahn <changhoon.hahn@princeton.edu>
2021-05-21 14:00:12 -0400

    make random revisited multiple times to check it's correct --- it is

--- bin/make_random.py
+++ bin/make_random.py
@@ -8,7 +8,8 @@ import os
 import h5py 
 import numpy as np 
 
-from boss_sbi import forwardmodel as FM 
+import pymangle 
+from simbig import forwardmodel as FM 
 
 def random(): 
     ''' construct hdf5 for random catalog of RA and Dec from ransack output 
@@ -28,18 +29,33 @@ def random():
 def random_veto():
     ''' construct random catalog of RA, Dec with veto mask applied
     '''
-    frand = h5py.File(os.path.join(os.environ['QUIJOTE_DIR'], 'chang', 'random_DR12v5_LOWZ_South.hdf5'), 'r')
-    rand_ra = frand['ra'][...]
-    rand_dec = frand['dec'][...]
-    
-    # identify points in veto mask 
-    in_veto = FM.BOSS_veto(rand_ra, rand_dec)
+    fvetos = [
+            'badfield_mask_postprocess_pixs8.ply', 
+            'badfield_mask_unphot_seeing_extinction_pixs8_dr12.ply',
+            'allsky_bright_star_mask_pix.ply',
+            'bright_object_mask_rykoff_pix.ply', 
+            'centerpost_mask_dr12.ply', 
+            'collision_priority_mask_dr12.ply']
 
-    # save points outside of veto mask to hdf5 file  
-    f = h5py.File(os.path.join(os.environ['QUIJOTE_DIR'], 'chang', 'random_DR12v5_LOWZ_South.veto.hdf5'), 'w')
-    f.create_dataset('ra', data=rand_ra[~in_veto]) 
-    f.create_dataset('dec', data=rand_dec[~in_veto]) 
-    f.close()
+    veto_dir = os.path.join(os.environ['QUIJOTE_DIR'], 'chang', 'simbig')
+    for i, fveto in enumerate(fvetos): 
+        if i == 0:
+            frand = h5py.File(os.path.join(os.environ['QUIJOTE_DIR'], 'chang', 'random_DR12v5_LOWZ_South.hdf5'), 'r')
+        else: 
+            frand = h5py.File(os.path.join(os.environ['QUIJOTE_DIR'], 'chang', 'random_DR12v5_LOWZ_South.veto.hdf5'), 'r')
+        rand_ra = frand['ra'][...]
+        rand_dec = frand['dec'][...]
+        frand.close() 
+
+        veto = pymangle.Mangle(os.path.join(veto_dir, fveto))
+        w_veto = veto.weight(rand_ra, rand_dec)
+        in_veto = (w_veto > 0.)
+
+        # save points outside of veto mask to hdf5 file  
+        f = h5py.File(os.path.join(os.environ['QUIJOTE_DIR'], 'chang', 'random_DR12v5_LOWZ_South.veto.hdf5'), 'w')
+        f.create_dataset('ra', data=rand_ra[~in_veto]) 
+        f.create_dataset('dec', data=rand_dec[~in_veto]) 
+        f.close()
     return None 
 
 
